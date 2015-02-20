e9f26b286c126728bdf572a54f213574afda2f13
Konstantin Cherkashin <konstantin.cherkashin@symmetrics.de>
2015-02-20 17:33:34 +0200

    controller cs fix

--- src/xogame/controller.clj
+++ src/xogame/controller.clj
@@ -1,11 +1,7 @@
 (ns xogame.controller
-  (:use [xogame.model :only [game-field tiles]]
-        [xogame.core :only [main-frame my-content]]
-        [seesaw.core]))
+  (:use [xogame.model :only [game-field tiles]]))
 
 (defn make-move [x y turn]
-  (swap! game-field update-in [x y] (fn [a] (tiles turn)))
-  (if (= @turn :cross) (swap! turn (fn [a] :zero))
-      (swap! turn (fn [a] :cross)))
-  (config! main-frame :content (my-content))
-    (alert x "tes"))
+  (do (swap! game-field update-in [x y] (fn [a] (tiles @turn)))
+      (if (= @turn :cross) (swap! turn (fn [a] :zero))
+          (swap! turn (fn [a] :cross)))))
