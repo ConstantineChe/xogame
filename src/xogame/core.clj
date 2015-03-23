38467d5f662bf67e3c94a77d8562fc99cbdb8856
Constantine Cherkashin <che.constantine@gmail.com>
2015-03-23 11:58:38 +0000

    implemented line check

--- src/xogame/core.clj
+++ src/xogame/core.clj
@@ -1,19 +1,25 @@
 (ns xogame.core
   (:use [seesaw.core :as s]
-        [xogame.model :only [game-field field-size tiles]]))
+        [xogame.model :only [game-field field-size tiles]])
+  (:require [clojure.math.combinatorics :as comb]))
 
-(defn -main []
-  (print *ns*)
-  (in-ns 'xogame.core)
-  (print *ns*))
+(declare main-frame
+         add-buttons
+         make-move
+         is-finished?)
 
 (defn main []
   (-> main-frame pack! show!))
 
+(defn -main []
+  (in-ns 'xogame.core)
+  (main))
+
+
+
 (defn my-content []
   (s/horizontal-panel :items (add-buttons field-size)))
 
-(def main-frame (s/frame :title "XO", :on-close :hide, :content (my-content)))
 
 (def turn (atom :cross))
 
@@ -38,4 +44,24 @@
    (do (swap! game-field update-in [x y] (fn [a] (tiles @turn)))
        (if (= @turn :cross) (swap! turn (fn [a] :zero))
            (swap! turn (fn [a] :cross)))
-       (config! main-frame :content (my-content)))))
+       (config! main-frame :content (my-content))
+       (is-finished? [x y]))))
+
+(def main-frame (s/frame :title "XO", :on-close :hide, :content (my-content)))
+
+(defn check-direction [current direction counter reverse?]
+  "check for line in direction"
+  (let [next (map (fn [x y] (+ x y)) current direction)]
+      (if (= (get-in @game-field current)
+             (get-in @game-field next))
+        (recur next direction (inc counter) false)
+        (if (not reverse?) (recur current (map #(* % -1) direction) 1 true)
+            counter))))
+
+(defn is-finished? [current]
+  "check if line is done."
+  (if (some #(> % 4) (map
+             (fn [direction]
+               (check-direction current direction 1 false))
+             (map vec (comb/cartesian-product [1 0] [0 1]))))
+    (s/alert "done")))
