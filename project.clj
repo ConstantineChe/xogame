97616fe0030bd27cbad3622c40da3736d620ff2c
Constantine Cherkashin <che.constantine@gmail.com>
2015-03-23 17:58:19 +0000

    add combinatorics to project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.5"]]
+                 [seesaw "1.4.5"]
+                 [org.clojure/math.combinatorics "0.1.1"]]
   :main xogame.core)
